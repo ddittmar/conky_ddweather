d2995b24b2a10f840f93391e46e9ec725fe33378
Dirk Dittmar <dirk.dittmar@web.de>
2016-05-29 08:07:29 +0200

    README corrected

--- ddweather.lua
+++ ddweather.lua
@@ -5,8 +5,6 @@
 --  License : Distributed under the terms of GNU GPL version 2 or later
 --
 --==============================================================================
--- TODO <div>Icons made by <a href="http://www.flaticon.com/authors/madebyoliver" title="Madebyoliver">Madebyoliver</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
-
 
 require 'cairo'
 JSON = require ("dkjson")
